9af48e7f9cfdfe74b86c2cdea637ebd7c7ce6e68
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 19:54:23 +0200

    Add files via upload

---
+++ 10/10/10/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intGetal1, intGetal2;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intGetal1 = Convert.ToInt32(tbGetal1.Text);
+            intGetal2 = Convert.ToInt32(tbGetal2.Text);
+
+            if(intGetal1 % 2 == 0 && intGetal2 % 2 == 0)
+            {
+                lblAllebeiEvenOfNiet.Text = "Allebei even.";
+            }
+
+            else if(intGetal1 % 2 != 0 && intGetal2 % 2 != 0)
+            {
+                lblAllebeiEvenOfNiet.Text = "Allebei oneven.";
+
+            }
+        }
+    }
+}
---
+++ 11/11/11/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double intTeller = 0, intTeller2, intInvoer;
+
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt16(tbInvoer.Text);
+
+            //string binary = Convert.ToString(intInvoer, 2);
+
+
+            //tbUitvoer.Text = binary;
+
+            while (Math.Pow(2, intTeller) < intInvoer)
+            {
+                intTeller++;
+            }
+
+            rtTest.Text = intTeller.ToString();
+
+
+            /*if (Math.Pow(2, intTeller) == intInvoer)
+            {
+                tbUitvoer.Text += 1.ToString();
+                do 
+                {
+                    tbUitvoer.Text += 0.ToString();
+                    intTeller2++;
+                }while (intTeller2 < intTeller) ;
+            }
+            */
+            do
+            {
+
+                if (intInvoer - Math.Pow(2, intTeller - 1) == 0)
+                {
+                    tbUitvoer.Text += 1.ToString();
+                    intInvoer = intInvoer - Math.Pow(2, intTeller - 1);
+                    for (intTeller2 = 1; intTeller2 <= intTeller - 3; intTeller2++)
+                    {
+                        tbUitvoer.Text += 0.ToString();
+                    }
+                }
+
+                else if (intInvoer - Math.Pow(2, intTeller - 1) > 0)
+                {
+                    tbUitvoer.Text += 1.ToString();
+                    intInvoer = intInvoer - Math.Pow(2, intTeller - 1);
+                }
+
+
+                else if (intInvoer - Math.Pow(2, intTeller - 1) < 0)
+                {
+                    tbUitvoer.Text += 0.ToString();
+                }
+
+
+                intTeller--;
+
+            } while (intTeller > 0);
+        }
+    }
+}
+
